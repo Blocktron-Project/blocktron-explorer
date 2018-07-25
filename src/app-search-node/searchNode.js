302c1d4bd04892c29f694852dfd9b768646e7163
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-25 21:10:15 +0530

    updated

--- src/app-search-node/searchNode.js
+++ src/app-search-node/searchNode.js
@@ -8,23 +8,26 @@ class Header extends Component {
 
   constructor() {
     super()
+    this.state = {
+      searchInput: 'validate',
+      searchButton: 'waves-effect btn lime white-text disabled'
+    };
   }
 
-  searchHandler() {
-    let query = document.querySelector('#search');
+  validate = () => {
+    let input = document.querySelector('#search');
+    let button = document.querySelector('#button');
     let url = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$/;
-    let validUrl;
-    if (!query.value.match(url)) {
-      if (!query.classList.contains('invalid')) {
-        query.classList += ' invalid';
-        query.classList.remove('valid');
-      }
-    } else if (query.value.match(url)) {
-      if (query.classList.contains('invalid')) {
-        query.classList.remove('invalid');
-        validUrl = query.value;
-        console.log(query.value);
-      }
+    if (input.value.match(url)) {
+      this.setState({
+        searchInput: 'validate valid',
+        searchButton: 'waves-effect btn lime white-text'
+      });
+    } else {
+      this.setState({
+        searchInput: 'validate invalid',
+        searchButton: 'waves-effect btn lime white-text disabled'
+      });
     }
   };
 
@@ -36,10 +39,10 @@ class Header extends Component {
             <div className="card-content white-text">
               <div className="row">
                 <div className="input-field col s8 m10 l11">
-                  <input id="search" type="text" className="validate" />
+                  <input id="search" type="text" className={this.state.searchInput} onChange={this.validate} />
                   <label htmlFor="search">Search Node</label>
                 </div>
-                <a className="waves-effect btn lime white-text" onClick={this.searchHandler}>
+                <a id="button" className={this.state.searchButton}>
                   <i className="material-icons">search</i></a>
               </div>
             </div>
