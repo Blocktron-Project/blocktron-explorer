c6324aece688521b9e9e96dfee68bd1ad2ffe968
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-25 20:15:17 +0530

    edits

--- src/app-search-node/searchNode.js
+++ src/app-search-node/searchNode.js
@@ -8,20 +8,22 @@ class Header extends Component {
 
   constructor() {
     super()
-
   }
 
   searchHandler() {
     let query = document.querySelector('#search');
     let url = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$/;
+    let validUrl;
     if (!query.value.match(url)) {
       if (!query.classList.contains('invalid')) {
         query.classList += ' invalid';
         query.classList.remove('valid');
       }
-    } else {
+    } else if(query.value.match(url)){
       if (query.classList.contains('invalid')) {
         query.classList.remove('invalid');
+        validUrl = query.value;
+        console.log(query.value);
       }
     }
   };
