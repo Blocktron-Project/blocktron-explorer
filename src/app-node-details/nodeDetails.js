ea0767fc590f5bf824b39c16caa41d1cd46080cb
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-27 20:49:44 +0530

    updates

--- src/app-node-details/nodeDetails.js
+++ src/app-node-details/nodeDetails.js
@@ -9,22 +9,83 @@ class NodeDetails extends Component {
     }
 
     render() {
-        if (this.props.nodeDetails) {
+        console.log(this.props);
+        if (this.props.nodeDetails && this.props.nodeDetails.data) {
+            let data = this.props.nodeDetails.data;
             return (
                 <div className="row">
                     <div className="col s12">
                         <div className="card">
                             <div className="card-content grey-text">
                                 <span className="card-title">
-                                    <div className="chip left lime">Node</div>127.0.0.1:3001
+                                    <div className="chip left lime">Node</div> {data.configuration.node_address}
                                 </span>
                             </div>
                             <div className="card-action grey-text">
-                                <p>I am a very simple card. I am good at containing small bits of information.</p>
-                            </div>
-                            <div className="card-action">
-                                <a href="#">This is a link</a>
-                                <a href="#">This is a link</a>
+                                <table className="striped highlight">
+                                    <tbody>
+                                        <tr>
+                                            <td>Status Message</td>
+                                            <td>{data.message}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Status Code</td>
+                                            <td>{data.status_code}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Port</td>
+                                            <td>{data.port}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Process Title</td>
+                                            <td>{data.configuration.process_title}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Process Id</td>
+                                            <td>{data.configuration.process_pid}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Memory: Resident Set Size</td>
+                                            <td>{data.configuration.memory.resident_set_size}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Memory: Heap Total</td>
+                                            <td>{data.configuration.memory.heap_total}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Memory: Heap Used</td>
+                                            <td>{data.configuration.memory.heap_used}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Memory: External</td>
+                                            <td>{data.configuration.memory.external}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Node Id </td>
+                                            <td>{data.configuration.node_id}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Environment</td>
+                                            <td>{data.configuration.environment}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Operating System</td>
+                                            <td>{data.configuration.os}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>CPU Architecture</td>
+                                            <td>{data.configuration.cpu_arch}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Node Version</td>
+                                            <td>{data.configuration.process_versions.node_version}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>V8 Version</td>
+                                            <td>{data.configuration.process_versions.v8_version}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
