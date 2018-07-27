e6669fc2d9d8f2ef4577092385f1b612b900334d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-27 21:33:29 +0530

    bttoast added to main file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './app-header/header';
-import Body from './app-body/body'
+import Body from './app-body/body';
+import btToast from './lib/btToast';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(< Header />, document.getElementById('header'));
