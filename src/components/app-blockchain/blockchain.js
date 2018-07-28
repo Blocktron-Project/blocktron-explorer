0ea8f3e33f6912e95af864730f8e1e2aa317689e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-28 14:17:22 +0530

    blockchain ui

--- src/components/app-blockchain/blockchain.js
+++ src/components/app-blockchain/blockchain.js
@@ -2,18 +2,39 @@ import React, {
     Component
 } from 'react';
 import './style.css';
+import axios from 'axios';
 
 class Blockchain extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            chainData: ''
+        };
+    }
+
+    componentWillMount(){
+        if(this.props && this.props.node){
+            let nodeUrl = this.props.node;
+            axios.get(nodeUrl+'/blockchain')
+            .then((response)=>{
+               this.state.chainData = response.data;
+            })
+            .catch((error) => {
+                _bt.btToast('Node not accessible!', { level: 'error' });
+            });
+        }
+    }
+
     render() {
         return (
             <div className="row">
-                <div class="col s12 m6">
-                    <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <span class="card-title">Card Title</span>
+                <div className="col s12 m6 l6">
+                    <div className="card">
+                        <div className="card-content grey-text">
+                            <span className="card-title">Blockchain</span>
                             <p>I am a very simple card. I am good at containing small bits of information.</p>
                         </div>
-                        <div class="card-action">
+                        <div className="card-action">
                             <a href="#">This is a link</a>
                             <a href="#">This is a link</a>
                         </div>
