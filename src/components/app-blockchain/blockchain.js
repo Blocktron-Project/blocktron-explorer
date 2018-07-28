1bfee181257c330a467590fe5ad2b4768dfdfc59
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-28 13:51:23 +0530

    updates on components

---
+++ src/components/app-blockchain/blockchain.js
@@ -0,0 +1,27 @@
+import React, {
+    Component
+} from 'react';
+import './style.css';
+
+class Blockchain extends Component {
+    render() {
+        return (
+            <div className="row">
+                <div class="col s12 m6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">Card Title</span>
+                            <p>I am a very simple card. I am good at containing small bits of information.</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">This is a link</a>
+                            <a href="#">This is a link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Blockchain;
