42e1317a00664dfe8c07c52055767deb94464921
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-28 21:15:09 +0530

    updates

--- src/components/app-body/body.js
+++ src/components/app-body/body.js
@@ -14,9 +14,8 @@ class Body extends Component {
     }
 
     componentDidMount() {
-        $(document).ready(function () {
-            $('.tooltipped').tooltip();
-        });
+        $('.tooltipped').tooltip();
+        $('.tabs').tabs();
     }
 
     getNodeDetails = (data) => {
@@ -57,23 +56,43 @@ class Body extends Component {
 
 
                 <div className="row">
-                    <div className="col s12 m6 offset-m3">
+                    <div className="col s12 m9 offset-m2">
                         <div className="card">
                             <div className="card-content grey-text">
                                 <span className="card-title"><div className="chip lime left">
                                     Node
                                 </div>http://127.0.01:3001</span>
-                                <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
                             </div>
-                            <div className="card-action">
-                                <a href="#">This is a link</a>
-                                <a href="#">This is a link</a>
+                            <div className="card-tabs">
+                                <ul className="tabs tabs-fixed-width">
+                                    <li className="tab"><a className="active" href="#overview">Overview</a></li>
+                                    <li className="tab"><a href="#configuration">Configuration</a></li>
+                                    <li className="tab"><a href="#memory">Memory & Resources</a></li>
+                                </ul>
+                            </div>
+                            <div className="card-content grey lighten-4">
+                                <div id="overview">Test 1</div>
+                                <div id="configuration">Test 2</div>
+                                <div id="memory">Test 3</div>
                             </div>
                         </div>
                     </div>
                 </div>
 
+                <div className="row">
+                    <div className="col s12 m9 offset-m2">
+                        <div className="card lime lighten-4">
+                            <div className="card-content grey-text">
+                                <span className="card-title">Genesis</span>
+                            </div>
+                            <div className="card-content grey-text">
+                            <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        </div>
+                       
+                    </div>
+                </div>
 
             </div>
 
