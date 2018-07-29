d22f4b46e1c6b5b02a2fc1a21c36f0efc654022e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-29 20:18:24 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/app-header/header';
 import Body from './components/app-body/body';
-import btToast from './lib/btToast';
+import lib from './lib/index';
 import registerServiceWorker from './registerServiceWorker';
 
 NProgress.configure({
@@ -11,6 +11,6 @@ NProgress.configure({
 });
 NProgress.start();
 
-ReactDOM.render( < Header / > , document.getElementById('header'));
-ReactDOM.render( < Body / > , document.getElementById('body'));
+ReactDOM.render(< Header />, document.getElementById('header'));
+ReactDOM.render(< Body />, document.getElementById('body'));
 registerServiceWorker();
