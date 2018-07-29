dd1c37ab067b7b4f4146ef0623e9d5a602e39bdf
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-29 13:59:09 +0530

    updates

--- src/components/app-body/body.js
+++ src/components/app-body/body.js
@@ -11,7 +11,8 @@ class Body extends Component {
         super(props)
 
         this.state = {
-            NodeDetails: ''
+            NodeDetails: '',
+            selectedNode: ''
         };
     }
 
@@ -24,8 +25,7 @@ class Body extends Component {
 
     getNodeDetails = (data) => {
         this.setState({
-            NodeDetails: data,
-            selectedNode: ''
+            NodeDetails: data
         });
     };
 
@@ -39,33 +39,7 @@ class Body extends Component {
                 <Welcome />
                 <AddNode getNodeDetails={this.getNodeDetails} />
                 <NodeChip nodeDetails={this.state.NodeDetails} handleChipClick={this.handleChipClick} />
-                <DetailsCard/>
-
-
-
-                <div className="row">
-                    <div className="col s12 m9 offset-m2">
-                        <div className="card hoverable">
-                            <div className="card-content grey-text">
-                                <span className="card-title"><div className="chip lime left">
-                                    Node
-                                </div>http://127.0.01:3001</span>
-                            </div>
-                            <div className="card-tabs">
-                                <ul className="tabs tabs-fixed-width">
-                                    <li className="tab"><a className="active" href="#overview">Overview</a></li>
-                                    <li className="tab"><a href="#configuration">Configuration</a></li>
-                                    <li className="tab"><a href="#memory">Memory & Resources</a></li>
-                                </ul>
-                            </div>
-                            <div className="card-content grey lighten-4">
-                                <div id="overview">Test 1</div>
-                                <div id="configuration">Test 2</div>
-                                <div id="memory">Test 3</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <DetailsCard selectedNode={this.state.selectedNode}/>
 
                 <div className="row">
                     <div className="col s12 m9 offset-m2">
--- src/components/app-header/header.js
+++ src/components/app-header/header.js
@@ -9,7 +9,7 @@ class Header extends Component {
       <div className="navbar-fixed" >
         <nav className="white" >
           <div className="nav-wrapper" >
-            <a href="#!" className="brand-logo lime-text" > &nbsp; Blocktron </a>
+            <a href="#!" className="brand-logo lime-text" > &nbsp; Blocktron <span className="beta-badge right">Beta v0.0.1</span></a>
           </div>
         </nav>
       </div>
