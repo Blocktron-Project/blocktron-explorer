b43f2b86f1e3b098391aa734440572baaebf989d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-29 13:18:16 +0530

    updates

--- src/components/app-node-chip/nodeChip.js
+++ src/components/app-node-chip/nodeChip.js
@@ -11,13 +11,17 @@ class NodeChip extends Component {
         super(props)
     }
 
+    componentDidUpdate(){
+        $('.tooltipped').tooltip();
+    }
+
     render() {
         if (this.props && this.props.nodeDetails && this.props.nodeDetails.data) {
             let data = this.props.nodeDetails.data;
             return (
                 <div className="center">
-                    <div className="chip lime lighten-3 bold-text">
-                        <img src="img/node.png" alt="Contact Person" />
+                    <div className="chip lime lighten-3 bold-text tooltipped" data-position="bottom" data-tooltip="Click to view details">
+                        <img src="img/node.png" alt="Contact Person"/>
                         {data.configuration.node_address}
                         <i className="close material-icons">close</i>
                     </div>
