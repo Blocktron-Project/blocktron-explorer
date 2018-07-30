c187c3239318cb0dbb735ba9dea8b28a447c255d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-30 14:43:19 +0530

    updates

--- src/components/app-chart-donut/chart.js
+++ src/components/app-chart-donut/chart.js
@@ -4,43 +4,34 @@
 import React, {
     Component
 } from 'react';
+import ReactDOM from 'react-dom';
 import './style.css';
 
 class DonutChart extends Component {
     constructor(props) {
         super(props)
-
-        this.state = {
-            chartData: '',
-            labels: ''
-        };
     }
 
     componentDidMount() {
-        let chart = document.querySelector('#chart');
-        console.log(this.state);
-        this.setState({
-            chartData: this.props.chartData,
-            labels: this.props.labels
-        });
+        let el = ReactDOM.findDOMNode(this.refs.chart);
+        let chart = el.getContext('2d');
         let chartData = {
             datasets: [{
-                data: [this.state.chartData[0], this.state.chartData[1]],
+                data: [parseInt(this.props.chartData[0]), parseInt(this.props.chartData[1])],
                 backgroundColor: ['#dce775', '#e6ee9c'],
                 hoverBackgroundColor: ['#afb42b', '#cddc39']
             }],
-            labels: [this.state.labels[0], this.state.labels[1]]
+            labels: [this.props.labels[0], this.props.labels[1]]
         };
-        let heapTotalChart = new Chart(chart, {
+        let drawChart = new Chart(chart, {
             type: 'doughnut',
             data: chartData
         });
     }
-
     render() {
         return (
             <div className="col s12 m6">
-                <canvas id="chart" width="300" height="300"></canvas>
+                <canvas ref="chart" width="300" height="300"></canvas>
             </div>
         );
     }
