3b26742de67e628a046e0af6a809944915cc77ae
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-07-31 13:41:17 +0530

    updates

--- src/components/app-chain-config/chainConfig.js
+++ src/components/app-chain-config/chainConfig.js
@@ -41,6 +41,8 @@ class ChainConfig extends Component {
 
     render() {
         if (this.state && this.state.pendingTransactions && this.state.networkNodes) {
+            let pendingTransactions = this.state.pendingTransactions;
+            let networkNodes = this.state.networkNodes;
             return (
                 <div className="row">
                     <div className="col s12 m10 offset-m1">
@@ -55,8 +57,54 @@ class ChainConfig extends Component {
                                 </ul>
                             </div>
                             <div className="card-content grey lighten-4">
-                                <div id="pendingTransactions">Test 1</div>
-                                <div id="networkNodes">Test 2</div>
+                                <div id="pendingTransactions">
+                                    {
+                                        pendingTransactions.length !== 0
+                                            ? <table className="striped highlight">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Transaction Id</th>
+                                                        <th>Amount</th>
+                                                        <th>Sender</th>
+                                                        <th>Receiver</th>
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody>
+                                                    {
+                                                        pendingTransactions.map(transaction => {
+                                                            return (
+                                                                <tr>
+                                                                    <td>{transaction.transactionId}</td>
+                                                                    <td>{transaction.amount}</td>
+                                                                    <td>{transaction.sender}</td>
+                                                                    <td>{transaction.receiver}</td>
+                                                                </tr>
+                                                            )
+                                                        })
+                                                    }
+                                                </tbody>
+                                            </table>
+                                            : <p className="grey-text">No pending transactions are found.</p>
+                                    }
+                                </div>
+                                <div id="networkNodes">
+                                    {networkNodes.length !== 0
+                                        ?
+                                        <table className="striped highlight">
+                                            <tbody>
+                                                {networkNodes.map(node => {
+                                                    return (
+                                                        <tr>
+                                                            <td>{node}</td>
+                                                        </tr>
+                                                    )
+                                                })}
+                                            </tbody>
+                                        </table>
+                                        : <p className="grey-text">No nodes have been added.</p>
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
