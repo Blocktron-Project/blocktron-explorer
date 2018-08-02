759424c560fada92bc4b458ca4a3f7a39568e80d
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-02 20:19:15 +0530

    new component added

---
+++ src/components/app-add-network-nodes/addNetworkNodes.js
@@ -0,0 +1,15 @@
+import React, {
+    Component
+} from 'react';
+import './style.css';
+
+class AddNetworkNode extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+    }
+}
+export default AddNetworkNode;
