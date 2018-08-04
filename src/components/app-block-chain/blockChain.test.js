09ef2be2afe3d724ae363965359f13ce571d6160
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-04 14:36:20 +0530

    test fixes for nprogress miss

--- src/components/app-block-chain/blockChain.test.js
+++ src/components/app-block-chain/blockChain.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './blockChain';
+import lib from './lib/index';
+
+if (!window.NProgress) {
+  window.NProgress = require('../../../public/js/nprogress');
+}
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render( <App /> , div);
   ReactDOM.unmountComponentAtNode(div);
 });
