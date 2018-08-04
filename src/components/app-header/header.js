4366f6709b4cc7e9f748b24a427590a0c819c9f5
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-04 21:10:50 +0530

    updates

--- src/components/app-header/header.js
+++ src/components/app-header/header.js
@@ -9,7 +9,7 @@ class Header extends Component {
       <div className="navbar-fixed" >
         <nav className="white" >
           <div className="nav-wrapper" >
-            <a href="#!" className="brand-logo lime-text" > &nbsp; Explorer
+            <a href="#!" className="brand-logo lime-text" >&nbsp; Blocktron Explorer
             </a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
               <li><a className="orange-text">Beta v0.0.1</a></li>
